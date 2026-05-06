e4d5ae57fb2b0d8c8526156b2e862dbf035c92f7
Vincent kauta <bsc-com-ne-07-24@unima.ac.mw>
2026-05-06 11:24:19 +0200

    my daily practice problems

---
+++ IntroToPointer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ int main() {
+   //declare pointer and initialize it
+   // so that it doesnt store a random address
+   int *ptr = nullptr;
+
+   int integerVar = 5;
+   //assign pointer to address of object
+   pPointer = &integerVar;
+   //output the value of intergerVar using the pointer
+   cout << "integervar: " << integerVar << endl;
+ }
